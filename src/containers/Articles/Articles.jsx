7b38017928947323ad34511d57af412d0aa45646
Boris Tenkes <boris.tenkes@gmail.com>
2023-01-21 20:49:36 +0100

    Articles done

--- src/containers/Articles/Articles.jsx
+++ src/containers/Articles/Articles.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
 import './Articles.scss';
+import { articles } from '../../constants';
 
 const Articles = () => {
   return (
-    <section>
-      Latest Articles
+    <section className='container'>
+      <h1 className="headtext !text-4xl mb-12">Latest Articles</h1>
+
+      <ul className='grid sm:grid-cols-2 lg:grid-cols-4 gap-10'>
+        {articles.map((article, index) => (
+          <li className='rounded-md overflow-hidden' key={`article-${index + 1}`}>
+            <img className='lg:h-44 h-64 object-cover w-full' src={article.image} alt={article.title} />
+            <div className='p-10 lg:p-4'>
+              <h3 className='headtext !text-xl !text-left mb-2 
+                             before:content-["By_"attr(data-author)] before:block before:text-xs before:text-neutral-400 before:mb-2' data-author={article.author}>
+                {article.title}
+              </h3>
+              <p className='body-text !text-left !m-0 lg:text-sm'>
+                {article.content}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </section>
   )
 }
